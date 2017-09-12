93d844b08a1a5e548e4be52a201409f5435898a7
Therwig6 <31357508+Therwig6@users.noreply.github.com>
2017-09-12 13:44:50 -0600

    Initial commit

---
+++ For-terations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Forterations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //this code writes i every time 1 is added
+            for (int i = 0; i < 10; i++) 
+            {
+                //Console.WriteLine(i.ToString());
+
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven");
+                    break;
+                    //This code ends the for iteration stoppin it at 7
+
+                }
+
+               
+            }
+            //This code writes my value as substituded for i
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
